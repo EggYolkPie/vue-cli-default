620be55133bbd03a1b7f34235a8020fec167fa33
369028414@qq.com <369028414@qq.com>
2019-11-06 16:02:21 +0800

    提交test1.html

--- src/entry-client.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from './main'
-const { app, router, store } = createApp()
-if (window.__INITIAL_STATE__) {
-    store.replaceState(window.__INITIAL_STATE__)
-}
-router.onReady(() => {
-    app.$mount('#app')
-})
